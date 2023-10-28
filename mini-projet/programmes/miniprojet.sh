8e5ae6b6e475273178dd2281adcb35534e74465c
PatriciaAugustyn <patricia.augustyn@sorbonne-nouvelle.fr>
2023-10-28 17:28:25 +0200

    Ajout du mini-projet

---
+++ mini-projet/programmes/miniprojet.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+if [ $# -ne 1 ]
+then
+	echo "Un argument attendu exactement"
+	exit
+else
+	if [ -f ../urls/$1 ]
+	then
+		echo "Il faut chosir un fichier"
+	else
+		echo "Le fichier n'existe pas"
+		exit
+	fi
+fi
+
+fichierURL=$1
+lineno=0
+
+while read -r line;
+
+do
+	code=$(curl -I -s ${line} | egrep -i "HTTP/")
+	encodage=$(curl -s -s  ${line} | egrep -i "charset=")
+
+	echo -e ${lineno} "\t" ${line} "\t" $code "\t" ${encodage} "\t"
+	lineno=$(expr ${lineno} + 1)
+
+done <"../urls/$fichierURL";
